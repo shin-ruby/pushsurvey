7e757ac9dad310b7c8fe9648a87dac49c3519a86
unknown <DELL@.(none)>
2011-07-23 20:12:14 +0800

    public

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proedm::Application.config.secret_token = 'e64249ba421073595286dc9fa6e88bff7dec2c2624f8db463221196efc84ba42be124969160f50b25b78a39e560a0b365604aaf193d16b007b13333f76bf7c83'
