c0a49a7074bf6e9f07220d63984c94d4689b6599
femto <femtowin@gmail.com>
2011-07-29 01:43:40 +0800

    test

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -88,7 +88,7 @@ class ContactsController < ApplicationController
     #require 'roo'
     puts "do importing"
     #Excel.new(params[:xx])
-
-    redirect_to :action=>"import"
+    render :text =>       params[:xx].read
+    #redirect_to :action=>"import"
   end
 end
