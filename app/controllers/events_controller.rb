29e3493e2f790132caf0c09973211b8de22ca68e
femto <femtowin@gmail.com>
2011-10-15 01:27:25 +0800

    200 OK

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,13 @@
 class EventsController < InheritedResources::Base
   before_filter :authenticate_admin_user, :except => :postback
   def postback
-    #p params
-    event = Event.new(params)
+    p params
+    event = Event.new
+    event.event = params[:event]
+    event.email = params[:email]
+    event.category = params[:category]
     event.content = params
     event.save!
+    render :nothing => true
   end
 end
