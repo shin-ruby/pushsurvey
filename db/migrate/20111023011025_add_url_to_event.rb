dbfe91f0228185e27eb73997a6eb0cf00af5372c
femto <femtowin@gmail.com>
2011-10-23 09:27:20 +0800

    push

--- db/migrate/20111023011025_add_url_to_event.rb
+++ db/migrate/20111023011025_add_url_to_event.rb
@@ -2,7 +2,7 @@ class AddUrlToEvent < ActiveRecord::Migration
   def self.up
     add_column :events, :url, :string
 
-    add_index :contacts, [:address_book_id, :email]
+    add_index :contacts, [:address_book_id, :email]#, :unique => true
   end
 
   def self.down
