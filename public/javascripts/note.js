7cc644cf5298a291c048b0a733af8348dd6444b3
Shin <shin.php@gmail.com>
2011-11-20 15:04:27 +0800

    rm

--- public/javascripts/note.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$("#menu li").each(function(index) {
-							   $(this).click(function() {
-													  $("#menu li.tabFocus").removeClass("tabFocus");
-													  $(this).addClass("tabFocus");
-													  $("#content li:eq(" + index + ")").show().siblings().hide();
-													  });
-							   });
-})
