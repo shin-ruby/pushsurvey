567c38db901a14af252eeca794e4c22974f98759
femto <femtowin@gmail.com>
2011-11-21 14:36:29 +0800

    change 4.hours to 12.hours

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,5 +1,5 @@
 Delayed::Worker.destroy_failed_jobs = false
 #Delayed::Worker.sleep_delay = 60
 #Delayed::Worker.max_attempts = 3
-#Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.max_run_time = 12.hours
 Delayed::Worker.delay_jobs = !Rails.env.test?
