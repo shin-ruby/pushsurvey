0d6704cd22bd261cbd6c8262a49704bf249abee8
femto <femtowin@gmail.com>
2011-12-06 21:08:02 +0800

    weird

--- app/controllers/address_books_controller.rb
+++ app/controllers/address_books_controller.rb
@@ -82,7 +82,7 @@ class AddressBooksController < InheritedResources::Base
       if params[:add_contact]
         if params[:contacts].present? && params[:contacts].strip != "="
         #InlineCsvImporter.new(params[:contacts],@address_book).import
-          Delayed::Job.enqueue CsvImporter.new(@address_book, :string => params[:contacts])
+          CsvImporter.new(@address_book, :string => params[:contacts]).delay.import
         else
           flash[:notice] = "Please input information for importing contacts"
          redirect_to :action=>"import"
