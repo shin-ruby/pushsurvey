2ea86f1b0ead324a73ad2a616d8f1313e3788f8d
Shin <shin.php@gmail.com>
2011-12-06 12:02:45 +0800

    change start.html.erb and push_mailer.rb

--- app/mailers/push_mailer.rb
+++ app/mailers/push_mailer.rb
@@ -7,8 +7,8 @@ class PushMailer < ActionMailer::Base
     @from = push.from_email.present? && push.from_email.strip.present? ? push.from_email.strip : "admin@pushsurvey.com"
     @user = contact
     headers["X-SMTPAPI"] = "{\"category\": \"push-#{@push.id}\"}"
-    attachments.inline['logo.png'] = File.open('public/images/logo.png', "rb").read
-    attachments.inline['y.gif'] = File.open('public/images/y.gif', "rb").read
+    #attachments.inline['logo.png'] = File.open('public/images/logo.png', "rb").read
+    #attachments.inline['y.gif'] = File.open('public/images/y.gif', "rb").read
     options = {:to => contact.email, :from=>@from}
     options[:reply_to] = @push.reply_to_email if @push.reply_to_email.present? && @push.reply_to_email.strip.present?
 
