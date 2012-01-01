9b8557cfd7f1461c32f3edcd0efbff24c5c36318
femto <femtowin@gmail.com>
2012-01-01 20:53:59 +0800

    tmp uploads dir

--- app/uploaders/importer_uploader.rb
+++ app/uploaders/importer_uploader.rb
@@ -15,7 +15,7 @@ class ImporterUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "tmp/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     #"uploads/"
   end
 
