0c4daa0a56e10534d0ef9cd72f90fc796e31eeb7
femto <femtowin@gmail.com>
2012-01-01 21:02:16 +0800

    uploads to tmp dir

--- app/uploaders/importer_uploader.rb
+++ app/uploaders/importer_uploader.rb
@@ -15,8 +15,12 @@ class ImporterUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "tmp/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     #"uploads/"
+  end
+
+  def cache_dir
+    "#{RAILS_ROOT}/tmp/uploads"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
